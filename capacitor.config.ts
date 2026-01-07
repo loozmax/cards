0e39a85b9d1b1df9d3108064a6b53cf2cb9abd61
loozmax <62829571+loozmax@users.noreply.github.com>
2026-01-07 22:16:40 +0500

    Improve UI styling and add gitignore

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.durakbluetooth',
+  appName: 'Дурак Bluetooth',
+  webDir: 'dist/durak-bluetooth',
+  bundledWebRuntime: false
+};
+
+export default config;
